cd320fda1e8a9b5afc11692ca8918730e42c9e8e
Umesh Suwal <mail@umeshsuwal.com.np>
2023-12-28 07:05:02 +0545

    Create HexaToDecimal.cpp

---
+++ HexaToDecimal.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int dec = 0;
+	int x,y,z;
+	int i = 1;
+	int n = 0;
+	printf("Enter the Hexadecimal Value:");
+	scanf("%d",&x);
+	if((x % 10) <= 15)
+	{
+		printf("\nHere are the following Steps : ");
+	while(x != 0)
+	{
+		y = x % 10;
+		x = x / 10;
+		i = pow(16,n);
+		z = dec;
+		dec = dec + (y * i);
+		printf("\n%d + (%d * %d) = %d",z,y,i,dec);
+		n = n + 1;
+	}
+	printf("\n \nThe Decimal Representation is %d",dec);
+	}
+	else
+	{
+		printf("Wrong input !!!");
+	}
+	return 0;
+}
